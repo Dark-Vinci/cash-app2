5a2eb89b058507b588719a9ad9b705c659e5f8c7
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2023-10-15 21:24:11 +0100

    chore: more work to do

--- src/components/nav/stay-in-touch/stay-in-touch.tsx
+++ src/components/nav/stay-in-touch/stay-in-touch.tsx
@@ -1,6 +1,7 @@
 import { JSX } from 'react';
 import { VscTwitter } from 'react-icons/vsc';
 import { PiLinkedinLogoFill } from 'react-icons/pi'
+import { FaTwitch, FaInstagram } from 'react-icons/fa';
 
 import scss from './stay-in-touch.module.scss';
 
@@ -13,9 +14,13 @@ export function StayInTouch(): JSX.Element {
     return (
         <div className={scss.stay_in_touch}>
             <h3>Stay in touch</h3>
+
             <div className={scss.icon_container}>
                 <VscTwitter color='white'/>
                 <PiLinkedinLogoFill />
+                <FaTwitch />
+                <FaInstagram />
+                <BiLogoTiktok />
             </div>
         </div>
     );
