3274e08a3a4ef763cf3e74cc946b2c4eb2b96f44
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2023-10-16 09:16:53 +0100

    $1: $2

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
+module.exports = {
+  //other rules
+  resolve: {
+    plugins: [new TsconfigPathsPlugin()],
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+}
